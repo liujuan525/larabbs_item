358558e3b8d3c1a802d9cc478df5377acc900562
may <bingqing0822@163.com>
2020-05-24 14:39:45 +0800

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,7 +25,7 @@ class UsersController extends Controller
         /** 上传头像处理 */
         $data = $request->all();
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
